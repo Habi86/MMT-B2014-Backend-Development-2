20f3ec0cab4f8ac257ce6452dcbe6be2968b2ec2
Stefanie Habersatter <shabersatter.mmt-b2014@fh-salzburg.ac.at>
2016-05-18 15:19:41 +0200

    Base

--- stveraAP/index.js
+++ stveraAP/index.js
@@ -1,3 +1,7 @@
+<html>
+<head>
+<script>
+
 var express = require('express');
 var app = express();
 
@@ -8,3 +12,36 @@ app.get('/', function (req, res) {
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
+
+</script>
+</head>
+
+<body>
+
+</body>
+</head>
+
+
+/*
+const cluster = require('cluster');
+const http = require('http');
+const numCPUs = require('os').cpus().length;
+
+if (cluster.isMaster) {
+  // Fork workers.
+  for (var i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
+  // Workers can share any TCP connection
+  // In this case it is an HTTP server
+  http.createServer((req, res) => {
+    res.writeHead(200);
+    res.end('hello world\n');
+  }).listen(8000);
+}
+*/
